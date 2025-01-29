8da65532138a4bbc45cc3ba9fb1ced71bc44d63d
i-3 <31x1962@gmail.com>
2025-01-29 17:25:41 +0200

    no cors

--- server/server.js
+++ server/server.js
@@ -15,18 +15,18 @@ const app = express();
 //   })
 // );
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET',
-    'POST',
-    'PATCH',
-    'DELETE'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET',
+//     'POST',
+//     'PATCH',
+//     'DELETE'
+//   );
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.use(express.json());
 app.use('/record', records);
