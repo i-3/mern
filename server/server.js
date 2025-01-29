869aa597c4b39f123b3610f61ff013b0537d384c
i-3 <31x1962@gmail.com>
2025-01-29 17:38:26 +0200

    +cors()

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import records from './routes/record.js';
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+app.use(cors());
 // app.use(
 //   cors({
 //     origin: '*',
@@ -14,7 +15,6 @@ const app = express();
 //     exposedHeaders: ['Content-Type'],
 //   })
 // );
-
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', '*');
 //   res.setHeader(
