8dbedc5ded4286579fcaf0744964b82be7d33d20
i-3 <31x1962@gmail.com>
2025-01-29 16:58:50 +0200

    res.set

--- server/server.js
+++ server/server.js
@@ -5,15 +5,28 @@ import records from './routes/record.js';
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(
-  cors({
-    origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-    exposedHeaders: ['Content-Type'],
-  })
-);
+// app.use(
+//   cors({
+//     origin: '*',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//     exposedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET',
+    'POST',
+    'PATCH',
+    'DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use(express.json());
 app.use('/record', records);
