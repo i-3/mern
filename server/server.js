082f36cb59b937bad6f7929353ce5db3917b80ff
i-3 <31x1962@gmail.com>
2025-01-29 15:04:54 +0200

    .lv/

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import records from './routes/record.js';
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors({ origin: 'https://mern.1984.lv/' }));
+app.use(cors({ origin: 'https://mern.1984.lv' }));
 app.use(express.json());
 app.use('/record', records);
 
