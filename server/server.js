765612e8fec877b1471bde10cf3a70f29e467c86
i-3 <31x1962@gmail.com>
2025-01-29 16:36:49 +0200

    corsOpts

--- server/server.js
+++ server/server.js
@@ -5,7 +5,16 @@ import records from './routes/record.js';
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors({ origin: 'https://mern.1984.lv' }));
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    exposedHeaders: ['Content-Type'],
+  })
+);
+
 app.use(express.json());
 app.use('/record', records);
 
